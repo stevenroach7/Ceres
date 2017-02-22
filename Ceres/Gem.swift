40e3b141d32528619b7dca9cbc78797745e9bb25
Daniel Ochoa <dochoa@macalester.edu>
2017-02-21 19:29:14 -0600

    Creates a gem class

--- Ceres/Gem.swift
+++ Ceres/Gem.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import GameplayKit
+
+class Gem: SKSpriteNode {
+    
+    private func dragSprite(currNode: SKNode, translation: CGPoint){
+        // Dragging functionality
+
+        let position = currNode.position
+
+        if currNode.name == "gem" {
+            currNode.position = CGPoint(x: position.x + translation.x, y: position.y + translation.y)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let positionInScene = touch.location(in: self)
+        let touchedNode = atPoint(positionInScene)
+        let previousPosition = touch.previousLocation(in: self)
+        let translation = CGPoint(x: positionInScene.x - previousPosition.x, y: positionInScene.y - previousPosition.y)
+
+
+        dragSprite(currNode: touchedNode, translation: translation)
+    }
+    
+}
