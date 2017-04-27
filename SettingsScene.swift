e2052f7b3671fc74fe822b152205d38bc17fb5de
seanacheng <scheng@macalester.edu>
2017-04-26 21:05:43 -0500

    creates another switch

--- SettingsScene.swift
+++ SettingsScene.swift
@@ -17,19 +17,22 @@ class SettingsScene: SKScene {
     var backButton = SKSpriteNode()
     let backButtonTex = SKTexture(imageNamed: "back")
     
-    let switchText = "Music"
-    let switchLabel = SKLabelNode(fontNamed: "Optima-Bold")
-    
+    let musicLabel = SKLabelNode(fontNamed: "Optima-Bold")
     var musicSwitch = UISwitch()
-    var switchIsOn = Bool()
+    
+    let soundLabel = SKLabelNode(fontNamed: "Optima-Bold")
+    var soundSwitch = UISwitch()
     
     var starfield:SKEmitterNode!
     
     override func didMove(to view: SKView) {
         /* Setup your scene here */
         
-        createSwitch(on: switchIsOn)
-        showSwitchLabel()
+        createMusicSwitch()
+        showMusicSwitchLabel()
+
+        createSoundSwitch()
+        showSoundSwitchLabel()
         
         titleNode.text = title
         titleNode.fontSize = 32
@@ -49,22 +52,14 @@ class SettingsScene: SKScene {
         starfield.zPosition = -1
     }
     
-    private func createSwitch(on: Bool) {
-        musicSwitch = UISwitch(frame:CGRect(x: frame.midX - size.width/20, y: frame.midY, width: 2, height: 2))
-        musicSwitch.setOn(on, animated: false)
-        musicSwitch.addTarget(self, action: #selector(switchValueDidChange(sender:)), for: .valueChanged)
+    public func createMusicSwitch() {
+        musicSwitch = UISwitch(frame: CGRect(x: frame.midX + size.width/10, y: size.height/4, width: 2, height: 2))
+        musicSwitch.setOn(true, animated: false)
+        musicSwitch.addTarget(self, action: #selector(musicSwitchOnOff(sender:)), for: .valueChanged)
         self.view!.addSubview(musicSwitch)
     }
     
-    private func showSwitchLabel() {
-        switchLabel.text = switchText
-        switchLabel.fontSize = 25
-        switchLabel.fontColor = SKColor.white
-        switchLabel.position = CGPoint(x: size.width/2 - size.width/5, y: size.height/2 - size.height/22)
-        addChild(switchLabel)
-    }
-    
-    func switchValueDidChange(sender:UISwitch!) {
+    func musicSwitchOnOff(sender:UISwitch!) {
         if sender.isOn == true {
             print("on")
         }
@@ -73,12 +68,36 @@ class SettingsScene: SKScene {
         }
     }
     
-    public func musicSwitchOnOrOff() {
-        if musicSwitch.isOn {
-            switchIsOn = true
-        } else {
-            switchIsOn = false
+    private func showMusicSwitchLabel() {
+        musicLabel.text = "Music"
+        musicLabel.fontSize = 25
+        musicLabel.fontColor = SKColor.white
+        musicLabel.position = CGPoint(x: size.width/2 - size.width/12, y: size.height * 3/4 - size.height/22)
+        addChild(musicLabel)
+    }
+    
+    public func createSoundSwitch() {
+        soundSwitch = UISwitch(frame: CGRect(x: frame.midX + size.width/10, y: size.height * 3/8, width: 2, height: 2))
+        soundSwitch.setOn(true, animated: false)
+        soundSwitch.addTarget(self, action: #selector(soundSwitchOnOff(sender:)), for: .valueChanged)
+        self.view!.addSubview(soundSwitch)
+    }
+    
+    func soundSwitchOnOff(sender:UISwitch!) {
+        if sender.isOn == true {
+            print("on")
         }
+        else {
+            print("off")
+        }
+    }
+    
+    private func showSoundSwitchLabel() {
+        soundLabel.text = "Sound Effects"
+        soundLabel.fontSize = 25
+        soundLabel.fontColor = SKColor.white
+        soundLabel.position = CGPoint(x: size.width/2 - size.width/5, y: size.height * 5/8 - size.height/22)
+        addChild(soundLabel)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -90,8 +109,8 @@ class SettingsScene: SKScene {
             //transitions back to menu screen if back button is touched
             if node == backButton {
                 if view != nil {
-                    musicSwitchOnOrOff()
                     musicSwitch.removeFromSuperview()
+                    soundSwitch.removeFromSuperview()
                     let transition:SKTransition = SKTransition.crossFade(withDuration: 1)
                     let scene:SKScene = MenuScene(size: self.size)
                     self.view?.presentScene(scene, transition: transition)
