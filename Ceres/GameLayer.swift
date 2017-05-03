7c331a040d8b12dacaa7b63032eacf3e17caf278
stevenroach7 <sroach@macalester.edu>
2017-05-03 11:59:13 -0500

    Adds some commennts explaining the situation

--- Ceres/GameLayer.swift
+++ Ceres/GameLayer.swift
@@ -32,4 +32,15 @@ class GameLayer: SKNode {
 //        }
 //    }
     
+    // When the application becomes active again, GameScene.isPaused becomes automatically set to false. This has a cascading effect where it also sets the isPaused on all child nodes of GameScene to false which is not what we want. We want the gameLayer isPaused to stay true so that the game can be in a paused state.
+    
+    // To fix this, I am creating another boolean variable on gameLayer to keep track of the true state of whether the scene should be paused.
+    // Changing this variable should also change gameLayer.isPaused in the same way.
+    // gameLayer.isPaused should not be able to be changed any other way and should always have the same value as gameLayer.gamePaused.
+    
+    
+    
+    
+    
+    
 }
